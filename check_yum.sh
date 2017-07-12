388119ed633d1266ec9a13d425233984891e9222
imnotme <ten2xv@protonmail.com>
2017-07-12 10:05:13 +0200

    Initial commit
    Check centos updates on local server.
    Lazy man way to do it, but it works.

---
+++ check_yum.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check for missing parameters
+YUMCHK=$(yum check-update)
+case $? in
+0)
+        echo "No system updates available"
+        exit 0
+        ;;
+100)
+        echo "System update available!"
+        echo $YUMCHK
+        exit 1
+        ;;
+*)
+        echo "Unknown exit code - Check manually"
+        exit 3
+        ;;
+esac
